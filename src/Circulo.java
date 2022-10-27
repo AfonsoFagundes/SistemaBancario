2bfd8d98fb3387c679713b8cff6f4a7d15927080
AfonsoFagundes <afonsodev21@gmail.com>
2022-10-27 17:58:12 -0300

    first commit

---
+++ src/Circulo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Circulo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+		int raio, diametro;
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.println("Digite o raio do circulo : ");
+		raio=ler.nextInt();
+		System.out.println("Digite o diametro : ");
+		diametro=ler.nextInt();
+		
+		
+		
+		
+		
+	}
+
+}
